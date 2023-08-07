9b93d1e3c2f54740cddb70b8f1aa0253b960c2f3
Jeong Euijin <speeduijin91@gmail.com>
2023-08-07 19:51:27 +0900

    chore(eslint): update parserOptions.project path

--- express/.eslintrc.cjs
+++ express/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'express/tsconfig.json',
   },
   ignorePatterns: ['.eslintrc.cjs', 'dist'],
-}
+};
