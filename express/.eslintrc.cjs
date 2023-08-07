f73bc0a763b91c24f9d46620570e1b27ed368253
Jeong Euijin <speeduijin91@gmail.com>
2023-08-07 15:20:29 +0900

    feat(express): Create Express.js API server boilerplate

---
+++ express/.eslintrc.cjs
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  ignorePatterns: ['.eslintrc.cjs', 'dist'],
+}
---
+++ express/src/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import logger from './logger';
+import app from './app';
+
+const PORT = process.env.PORT || 3000;
+const handleListening = () =>
+  logger.info(`Server listenting on http://localhost:${PORT}.`);
+
+app.listen(PORT, handleListening);
